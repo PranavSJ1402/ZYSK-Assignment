d82f3e4ba09efdc516ce963039d0f63d589f2146
PranavSJ1402 <123387225+PranavSJ1402@users.noreply.github.com>
2025-01-20 13:55:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@mui/icons-material']
+    include: ['@mui/icons-material'],
+      base: '/',
   },
 })
